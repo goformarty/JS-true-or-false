33b5d296b3685790ee56db3d83ef3858e66fea84
Marty Stepien <marty.stepien@gmail.com>
2018-02-26 14:55:23 +0000

    checking first question

--- quiz.js
+++ quiz.js
@@ -1,4 +1,21 @@
-let score = 0;
+let q1 = document.getElementById("q1");
+let answer = document.getElementById("answer");
 
-let scoreDisplay = document.getElementById("score");
-scoreDisplay.innerHTML = score;
+let condition1 = {};
+
+
+function checkAnswer(bool) {
+	let answer = document.getElementById("a1");
+	if (bool === isItTrue() ) {
+		a1.innerText = "Correct! :>";
+	} else {
+		a1.innerText = "Wrong! :<";
+	}
+}
+
+function isItTrue() {
+	if (condition1) {
+		return true;
+	}
+	return false;
+}
